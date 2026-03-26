60969bb4d5f565782faba8d0660790bc4bd834e0
AngelVillavicencio <villavicenciomunoz@gmail.com>
2026-03-26 11:33:01 -0500

    agregue otro products

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,15 @@ export default function Home() {
                 />
               ))}
             </SimpleGrid>
+            <SimpleGrid columns={{ base: 1, md: 2 }} gap={6} mt={10}>
+              {fakeProducts.map((product) => (
+                <ProductCard
+                  key={`${product.title}-${product.reviewCount}`}
+                  {...product}
+                  addToCarrito={() => addToCarrito(product)}
+                />
+              ))}
+            </SimpleGrid>
           </GridItem>
 
           <GridItem>
